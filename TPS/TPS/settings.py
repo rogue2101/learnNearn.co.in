083d045acb02220fb6e2162cebb6a7db7b24ab3c
rogue2101 <rajpriyasingh2002@gmail.com>
2022-10-18 15:27:05 +0530

    comit1

--- TPS/TPS/settings.py
+++ TPS/TPS/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eh@q5228igie8-_rr_x3lqf@(5d0llt67a20t01wx@_nzexs#t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://learnnnearn.herokuapp.com/']
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- TPS/TPS/urls.py
+++ TPS/TPS/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
 from TPS import views
+
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls, name= 'admin'),
     path('', views.HomePage, name= 'home'),
@@ -25,5 +31,8 @@ urlpatterns = [
     path('', include('placement_system.urls')),
     path('', include('zoom.urls')),
     path('', include('resume.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}) 
     
 ]
